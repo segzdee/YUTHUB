765f5b309004a43e77ff89928ff873354fc50c26
segzdee <segzdeee@gmail.com>
2025-10-30 07:13:00 -0700

    Updated index.html

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { initializePerformanceOptimizations } from '@/utils/performanceOptimization';
 import App from './App';
 import './index.css';
-
-// Initialize performance optimizations
-initializePerformanceOptimizations();
 
 createRoot(document.getElementById('root')!).render(<App />);
